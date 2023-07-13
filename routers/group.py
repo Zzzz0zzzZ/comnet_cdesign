1172e629cf2cbcc7d4e9636d61042f081e00c86e
chenlb <1796390642@qq.com>
2023-07-13 22:29:13 +0800

    fix: [routers]返回个人所有群组时返回群组的完整信息

--- routers/group.py
+++ routers/group.py
@@ -68,13 +68,14 @@ async def get_groups(uuid: str):
         groups = await UserGroup.filter(uuid=uuid)
         for group in groups:
             gid = group.gid
+            cur_group = await Group.get(gid=gid)
             members = await UserGroup.filter(gid=gid)
             members_info = []
             for member in members:
                 user = await User.get(uuid=member.uuid)
                 members_info.append(user)
             groups_info.append({
-                "gid": gid,
+                "group": cur_group,
                 "members": members_info
             })
         return response_msg("s", "获取成功", {
