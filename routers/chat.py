648b1c013d26b4abf2857b122a4e01ef66a22d69
chenlb <1796390642@qq.com>
2023-07-13 15:47:28 +0800

    feat: [routers]消息已读

--- routers/chat.py
+++ routers/chat.py
@@ -2,12 +2,13 @@ import json
 from typing import Dict
 from uuid import uuid4
 
+from pydantic import BaseModel
+
 from models.group import Group
 from models.user import User
 from models.msg_group import MsgGroup
 from models.user_group import UserGroup
 from fastapi import APIRouter
-from fastapi import WebSocket, WebSocketDisconnect
 from utils.response import response_ws
 from models.chat_msg import ChatMsg
 from utils.types import ChatType
@@ -15,6 +16,11 @@ from utils.response import response_msg
 from utils.pull_message import pull_single_message, pull_group_message
 
 router = APIRouter()
+
+
+class UpdateRead(BaseModel):
+    mid: str
+    is_read: int
 
 
 @router.post("/pull/{uuid}")
@@ -27,6 +33,18 @@ async def pull_message(uuid: str):
             "group_msg": group_msg
         }
         return response_msg("s", "获取成功", return_info)
+
+    except Exception as e:
+        raise {"msg": str(e), "data": None}
+
+
+@router.post("/update/read")
+async def update_read(chatParam: UpdateRead):
+    try:
+        msg = await ChatMsg.get(mid=chatParam.mid)
+        msg.is_read = chatParam.is_read
+        await msg.save()
+        return response_msg("s", "更改已读状态成功", msg)
 
     except Exception as e:
         raise {"msg": str(e), "data": None}
